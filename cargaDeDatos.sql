b2bf13155ae5430fae6dbfe507448dd586fba7a1
Josefina Montenegro <mariajosefina.mont@gmail.com>
2018-11-30 20:17:31 -0300

    Se agregan mozos y algunos menues promocionales

---
+++ cargaDeDatos.sql
@@ -0,0 +1,32 @@
+INSERT INTO Mozo (CUIL, nombreYApellido, telefono)
+VALUES ('27379765510', 'JuanPerez', '43052241');
+
+INSERT INTO Mozo (CUIL, nombreYApellido, telefono)
+VALUES ('27359765510', 'AnaDiaz', '43052671');
+
+INSERT INTO Mozo (CUIL, nombreYApellido, telefono)
+VALUES ('2629765510', 'MariaVazquez', '43612121');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('desayuno light', '2018-11-29', '2018-12-31', 'desayuno');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('desayuno americano', '2018-11-01', '2018-12-10', 'desayuno');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('almuerzo light', '2018-10-01', '2018-11-10', 'almuerzo');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('almuerzo americano', '2018-12-01', '2019-10-01', 'almuerzo');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('merienda light', '2018-11-25', '2018-12-25', 'merienda');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('merienda americana', '2018-11-22', '2019-03-08', 'merienda');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('cena light', '2018-12-02', '2019-02-22', 'cena');
+
+INSERT INTO MenuPromocional (nombre, desde, hasta, tipo)
+VALUES ('cena americana', '2018-11-01', '2019-02-25', 'cena');
