3a46fe9d9d2ebb7ad5e3cd9800022d1474a306cc
Christian Florez <cflorez@gde.gob.ar>
2018-12-02 16:11:36 -0300

    consulta 1 modificada

--- consultas.sql
+++ consultas.sql
@@ -1,8 +1,9 @@
 #Listado en pantalla de la cocina para ver pedidos pendientes
 #Tienen que salir distinguidos los pedidos que sean de menu a la carta y los que son de menu promocional
 #Pedidos pendientes ordenados por el número de mesa
-SELECT mesa, nombre FROM ItemCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral WHERE servido = false ORDER BY mesa ASC;
-SELECT mesa, nombreItem FROM ItemPedidoMenu WHERE servido = false ORDER BY mesa ASC;
+SELECT mesa, nombre, tipo FROM ItemCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral WHERE servido = false ORDER BY mesa ASC;
+
+SELECT mesa, nombre, tipo FROM ItemPedidoMenu NATURAL JOIN ItemMenuPromocional NATURAL JOIN ItemCartaGeneral WHERE servido = false ORDER BY mesa ASC;
 
 # Tendencias de opciones de menus promocionales. De cada menu promocional, que cantidades de platos se han pedido hasta ahora
 SELECT nombre, COUNT(*) as cantPlatos FROM ItemPedidoMenu NATURAL JOIN ItemMenuPromocional NATURAL JOIN ItemCartaGeneral GROUP BY idItem; 
