0bfee4e383465612c11118b32a950427df31dd90
Josefina Montenegro <mariajosefina.mont@gmail.com>
2018-12-02 14:09:25 -0300

    Se modifica query 3

--- consultas.sql
+++ consultas.sql
@@ -4,4 +4,4 @@
 
 
 # Tendencias de opciones de menus promocionales. De cada menu promocional, que cantidades de platos se han pedido hasta ahora
-SELECT nombreItem, COUNT(*) as cantPlatos FROM ItemPedidoMenu GROUP BY nombreItem; 
+SELECT nombre, COUNT(*) as cantPlatos FROM ItemMenuPromocional NATURAL JOIN ItemCartaGeneral GROUP BY idItem; 
