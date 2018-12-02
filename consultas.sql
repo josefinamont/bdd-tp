2b62d8ace6a35e24802f80dd7bcc259fdb991cc1
Franco Mazzoni <franco.mazzoni@despegar.com>
2018-12-02 16:07:59 -0300

    Primera parte de la segunda query

--- consultas.sql
+++ consultas.sql
@@ -5,12 +5,23 @@ SELECT mesa, nombre FROM ItemCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral WH
 SELECT mesa, nombreItem FROM ItemPedidoMenu WHERE servido = false ORDER BY mesa ASC;
 
 #Listado de todas las cosas pedidas por una mesa con los importes
+SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral 
+NATURAL JOIN ItemCartaGeneral  NATURAL JOIN PrecioItemCartaGeneral WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = 1 UNION
+SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoVino NATURAL JOIN ItemPedidoVino NATURAL JOIN ItemVino
+NATURAL JOIN Capacidad WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = 1 UNION
+SELECT fechaYHoraInicioServicio, mesa, nombreItem AS nombre, precio AS precioTotal FROM PrecioPedidoMenu NATURAL JOIN ItemPedidoMenu 
+NATURAL JOIN ItemMenuPromocional NATURAL JOIN Pasos WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = 1;
 
+#WHERE mesa = 3 AND fechaYHoraInicioServicio = '2018-11-30 10:30:00'
+
+SELECT mesa, nombre, precio FROM ItemPedidoCartaGeneral NATURAL JOIN ItemCartaGeneral NATURAL JOIN PrecioItemCartaGeneral;
+SELECT mesa, nombre, precio FROM ItemPedidoVino NATURAL JOIN ItemVino NATURAL JOIN Capacidad;
+SELECT mesa, nombre, precio FROM ItemPedidoMenu NATURAL JOIN ItemMenuPromocional NATURAL JOIN ItemCartaGeneral NATURAL JOIN PrecioItemCartaGeneral;
 
 #Calcular el total (factura, agregacion) → no es obligatorio en este caso listar que opcion pidio
 
 # Tendencias de opciones de menus promocionales. De cada menu promocional, que cantidades de platos se han pedido hasta ahora
-SELECT nombre, COUNT(*) as cantPlatos FROM ItemPedidoMenu NATURAL JOIN ItemMenuPromocional NATURAL JOIN ItemCartaGeneral GROUP BY idItem; 
+SELECT nombre, COUNT(*) as cantPlatos FROM ItemPedidoMenu NATURAL JOIN ItemMenuPromocional NATURAL JOIN ItemCartaGeneral GROUP BY idItem;
 
 # Idem pero con las opciones de carta
 SELECT nombre, COUNT(*) as cantPlatos FROM ItemPedidoCartaGeneral NATURAL JOIN ItemCartaGeneral GROUP BY nombre;
