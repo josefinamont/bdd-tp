3e4518b5fe902b9ba38955884f40a61dee6f6aae
Christian Florez <cflorez@gde.gob.ar>
2018-12-05 12:34:31 -0300

    se sube proceso para calcular la cuenta de una mesa, se pasa como parametro el numero de la mesa, faltarian pasar otros parametros que estan hardcodeados en la query

---
+++ procedure_calcularCuentaMesa.sql
@@ -0,0 +1,18 @@
+USE `restaurant`;
+DROP procedure IF EXISTS `calcularCuentaDeMesa`;
+
+DELIMITER $$
+USE `restaurant`$$
+CREATE PROCEDURE `calcularCuentaDeMesa`( numMesa INT )
+BEGIN  
+    SELECT fechaYHoraInicioServicio, mesa, SUM(precioTotal) AS precioFactura FROM (
+	SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral 
+	NATURAL JOIN ItemCartaGeneral  NATURAL JOIN PrecioItemCartaGeneral WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = numMesa UNION
+	SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoVino NATURAL JOIN ItemPedidoVino NATURAL JOIN ItemVino
+	NATURAL JOIN Capacidad WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = numMesa UNION
+	SELECT fechaYHoraInicioServicio, mesa, nombreItem AS nombre, precio AS precioTotal FROM PrecioPedidoMenu NATURAL JOIN ItemPedidoMenu 
+	NATURAL JOIN ItemMenuPromocional NATURAL JOIN Pasos WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = numMesa) 
+    AS PreciosPorPedido GROUP BY mesa, fechaYHoraInicioServicio;
+END$$
+
+DELIMITER ;
