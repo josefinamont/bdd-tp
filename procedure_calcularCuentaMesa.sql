c553215fa919de55f89a935ee4fa081e5e500803
Franco Mazzoni <franco.mazzoni@despegar.com>
2018-12-05 18:46:52 -0300

    Fix en otro procedure

--- procedure_calcularCuentaMesa.sql
+++ procedure_calcularCuentaMesa.sql
@@ -5,14 +5,15 @@ DELIMITER $$
 USE `restaurant`$$
 CREATE PROCEDURE `calcularCuentaDeMesa`( numMesa INT, fechaYHora datetime )
 BEGIN  
-    SELECT fechaYHoraInicioServicio, mesa, SUM(precioTotal) AS precioFactura FROM (
-	SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral 
+    SELECT SUM(precioTotal) AS precioFactura FROM (
+	SELECT nombre, cantidad*precio AS precioTotal FROM PrecioPedidoCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral 
 	NATURAL JOIN ItemCartaGeneral  NATURAL JOIN PrecioItemCartaGeneral WHERE fechaYHoraInicioServicio = fechaYHora AND mesa = numMesa UNION
-	SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoVino NATURAL JOIN ItemPedidoVino NATURAL JOIN ItemVino
+	SELECT nombre, cantidad*precio AS precioTotal FROM PrecioPedidoVino NATURAL JOIN ItemPedidoVino NATURAL JOIN ItemVino
 	NATURAL JOIN Capacidad WHERE fechaYHoraInicioServicio = fechaYHora AND mesa = numMesa UNION
-	SELECT fechaYHoraInicioServicio, mesa, nombreItem AS nombre, precio AS precioTotal FROM PrecioPedidoMenu NATURAL JOIN ItemPedidoMenu 
+	SELECT nombreItem AS nombre, precio AS precioTotal FROM PrecioPedidoMenu NATURAL JOIN ItemPedidoMenu 
 	NATURAL JOIN ItemMenuPromocional NATURAL JOIN Pasos WHERE fechaYHoraInicioServicio = fechaYHora AND mesa = numMesa) 
     AS PreciosPorPedido GROUP BY mesa, fechaYHoraInicioServicio;
 END$$
 
 DELIMITER ;
+
