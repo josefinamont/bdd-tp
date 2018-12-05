1c5aae05e27210e75131d5b3e2720e92f19ce2c6
Franco Mazzoni <franco.mazzoni@despegar.com>
2018-12-05 18:30:52 -0300

    Algunos fixes en un procedure

--- procedure_listarPedidosMesa.sql
+++ procedure_listarPedidosMesa.sql
@@ -6,11 +6,11 @@ USE `restaurant`$$
 CREATE PROCEDURE `listarPedidosDeMesa`( numMesa INT, fechaYHora datetime )
 BEGIN  
  SELECT nombre, cantidad*precio AS precioTotal FROM PrecioPedidoCartaGeneral NATURAL JOIN ItemPedidoCartaGeneral 
- NATURAL JOIN ItemCartaGeneral  NATURAL JOIN PrecioItemCartaGeneral WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = 1 UNION
- SELECT fechaYHoraInicioServicio, mesa, nombre, cantidad*precio AS precioTotal FROM PrecioPedidoVino NATURAL JOIN ItemPedidoVino NATURAL JOIN ItemVino
- NATURAL JOIN Capacidad WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = 1 UNION
- SELECT fechaYHoraInicioServicio, mesa, nombreItem AS nombre, precio AS precioTotal FROM PrecioPedidoMenu NATURAL JOIN ItemPedidoMenu 
- NATURAL JOIN ItemMenuPromocional NATURAL JOIN Pasos WHERE fechaYHoraInicioServicio = '2018-11-30 10:32:00' AND mesa = 1;
+ NATURAL JOIN ItemCartaGeneral  NATURAL JOIN PrecioItemCartaGeneral WHERE fechaYHoraInicioServicio = fechaYHora AND mesa = numMesa UNION
+ SELECT nombre, cantidad*precio AS precioTotal FROM PrecioPedidoVino NATURAL JOIN ItemPedidoVino NATURAL JOIN ItemVino
+ NATURAL JOIN Capacidad WHERE fechaYHoraInicioServicio = fechaYHora AND mesa = numMesa UNION
+ SELECT nombreItem AS nombre, precio AS precioTotal FROM PrecioPedidoMenu NATURAL JOIN ItemPedidoMenu 
+ NATURAL JOIN ItemMenuPromocional NATURAL JOIN Pasos WHERE fechaYHoraInicioServicio = fechaYHora AND mesa = numMesa;
 END$$
 
 DELIMITER ;
